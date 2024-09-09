2e2dbcef1460df9f91f05bfc6862168b269388af
Vaishnavi A K <97692276+vaishnavi-here-29@users.noreply.github.com>
2024-09-09 15:41:15 +0530

    Add files via upload
    uploaded all the files other than the node modules use npm commands to run the project

---
+++ BookNook/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://vaishnaviak098:vaishu098@book-app.o2ue5.mongodb.net/book-collection?retryWrites=true&w=majority&appName=book-app'
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
